929bbbb24ddef7098e71cfb917569b091780c26e
Petter Padilha <petterson_sorriso@hotmail.com>
2022-09-04 15:11:49 -0400

    criação da sidebar

---
+++ src/components/ServerList/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+    grid-area: SL;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--tertiary);
+    padding: 11px 0;
+    max-height:100vh;
+    
+    // Criando um stroll
+    overflow-y: scroll;
+    ::-webkit-scrollbar {
+    display: none;
+}
+`;
+
+export const Separator = styled.div `
+    // Criando um separador da barra lateral 
+
+`;
